fb1420c268001cd52880eeb2402d117caafd6156
Wunna Kyaw <heronskyaw@gmail.com>
2024-10-02 22:52:35 +0630

    update project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/route_manager.dart';
+import 'package:flutter_fortune_wheel/flutter_fortune_wheel.dart';
 import 'package:spin_around/firebase_options.dart';
-import 'package:spin_around/utils/constant/app_route.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,16 +11,56 @@ void main() async {
   runApp(const SpinAroundApp());
 }
 
-class SpinAroundApp extends StatelessWidget {
+class SpinAroundApp extends StatefulWidget {
   const SpinAroundApp({super.key});
 
   @override
+  State<SpinAroundApp> createState() => _SpinAroundAppState();
+}
+
+class _SpinAroundAppState extends State<SpinAroundApp> {
+  int count = 1;
+  List<FortuneItem> itemList = [
+    FortuneItem(child: Text('Han Solo')),
+    FortuneItem(child: Text('Yoda')),
+    FortuneItem(child: Text('Obi-Wan Kenobi')),
+  ];
+  void addItem() {
+    setState(() {
+      itemList.add(FortuneItem(child: Text('Item #$count')));
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Spin Around',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/auth',
-      getPages: AppRoute.getPages,
+      // initialRoute: '/auth',
+      home: Scaffold(
+        body: Column(
+          children: [
+            Expanded(
+              child: FortuneWheel(
+                physics: CircularPanPhysics(
+                  duration: Duration(seconds: 1),
+                  curve: Curves.decelerate,
+                ),
+                hapticImpact: HapticImpact.medium,
+                onFling: () {
+                  // controller.add(1);
+                },
+                // selected: controller.stream,
+                items: itemList,
+              ),
+            ),
+            ElevatedButton(
+              onPressed: addItem,
+              child: Text('Add Item'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
